98f18a934aafbc4d83eefb20f263f2ff0e3d45b1
apexneural-likhithmasura <likhith.masura@apexneural.com>
2025-11-18 11:35:31 +0530

    Create globalSlackNotifier.groovy

---
+++ vars/globalSlackNotifier.groovy
@@ -0,0 +1,14 @@
+def call(status, message) {
+
+    def color = (status == "SUCCESS") ? "good" :
+                (status == "FAILURE") ? "danger" :
+                (status == "UNSTABLE") ? "#FFA500" :
+                (status == "ABORTED") ? "#AAAAAA" :
+                "#439FE0"
+
+    slackSend(
+        channel: "jenkins-testing",
+        color: color,
+        message: "${env.JOB_NAME} (#${env.BUILD_NUMBER}) - ${message}\n${env.BUILD_URL}"
+    )
+}
